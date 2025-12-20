5bf3300d24c1d8964dfcb3dddadfa5b166f1c5cb
songkui <18303658792@163.com>
2025-12-20 22:17:47 +0800

    111

--- app.py
+++ app.py
@@ -207,7 +207,7 @@ def get_local_ip():
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Dataset Finder - Dataset Management Tool')
     parser.add_argument('--host', default='0.0.0.0', help='Host to bind to (default: 0.0.0.0 for LAN access)')
-    parser.add_argument('--port', type=int, default=5000, help='Port to bind to (default: 5000)')
+    parser.add_argument('--port', type=int, default=3000, help='Port to bind to (default: 5000)')
     parser.add_argument('--debug', action='store_true', help='Enable debug mode')
     
     args = parser.parse_args()
